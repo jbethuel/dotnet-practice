b5eb374d5e5a891f0d915289b25247c196bad869
Bethuel Dela Cruz <jbethueldc@gmail.com>
2023-05-30 21:34:11 +0800

    fix time

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!", DateTime.DaysInMonth(2));
+Console.WriteLine("Hello, World!" + " " + DateTime.Now);
